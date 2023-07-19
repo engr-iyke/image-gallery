8850df6a61edf32bc929adadd2aa86e9042a218b
Ikechukwu David <ikechukwud7@gmail.com>
2023-07-19 12:18:37 +0100

    App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
+// src/App.js
 import React from 'react';
+import MyTextComponent from './MyTextComponent';
 
-function App() {
+const App = () => {
+  const myText = 'Hello, this is my text from VSCode!';
+
   return (
-    <div className="App">
+    <div>
       <h1>Images Gallery</h1>
+      <MyTextComponent text={App} />
     </div>
- );
-}
+  );
+};
 
 export default App;
