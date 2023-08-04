0154dc4f0fae0096cf17f7ac27ba27ea19b51d1b
Ikechukwu David <ikechukwud7@gmail.com>
2023-08-04 01:19:58 +0100

    controlled commitment

--- frontend/src/Components/Search.js
+++ frontend/src/Components/Search.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 
-const Search = () => {
+const Search = ({ word, setword, handleSubmit }) => {
   return (
     <Container className="mt-4">
       <Row className="justify-content-center">
         <Col xs={12} md={8} lg={6}>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Row>
               <Col xs={9}>
-                <form.control placeholder="Search for image..." />
+                <form.control
+                    type="text"
+                    value={word}
+                    onChange={(e) => setWord(e.target.value)}
+                placeholder="Search for image..."
+                />
               </Col>
               <Col>
                 <Button variant="primary" type="submit">search</Button>
